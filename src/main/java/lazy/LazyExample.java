4c7099a25c53804f34ef641a0e9cd3aa6f79abcc
dervism <dervis4@gmail.com>
2023-04-27 17:34:33 +0200

    Updated example for lazy

---
+++ src/main/java/lazy/LazyExample.java
@@ -0,0 +1,26 @@
+package lazy;
+
+public class LazyExample {
+
+
+    public static int compute(int number) {
+        System.out.println("Called.");
+        return number * 2;
+    }
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        int temp = compute(5);
+        //var lazyTemp = Lazy.lazy(() -> compute(5));
+
+        System.out.println("Here.");
+
+        if  (x > 5 && temp > 10) {
+            System.out.println("Result.");
+        }
+        else
+            System.out.println("No result.");
+    }
+
+}

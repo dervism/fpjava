c1faf91057d56c799a44f2ffd7562588a9902da6
dervism <dervis4@gmail.com>
2024-05-15 18:20:41 +0200

    Updated partial fp example

--- src/main/java/functions/curried/CurriedPizzaFunctions.java
+++ src/main/java/functions/curried/CurriedPizzaFunctions.java
@@ -9,8 +9,11 @@ public class CurriedPizzaFunctions {
     //
     // NumPeopleSignedUp a, NumExtra b => (a, b) -> (a * 0.66 / 3) + b
 
-    interface DoubleFunction extends Function<Double, Double> {}
     interface CurriedFunction extends Function<Double, DoubleFunction> {};
+    interface DoubleFunction extends Function<Double, Double> {}
+    interface F3 extends Function<Double, Function<Double, DoubleFunction>> {};
+
+    F3 fn = a -> b -> c -> a + b + c;
 
     // function
     static DoubleFunction multiple66Percent = x -> x * 0.66;
@@ -26,7 +29,7 @@ public class CurriedPizzaFunctions {
 
 
     public static void main(String[] args) {
-        var numPizzas = peppes.apply(15d);
+        var numPizzas = peppes.apply(50d);
         System.out.printf("You need %.1f pizza's.", numPizzas).println();
     }
 }

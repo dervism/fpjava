3123145faa00589b2b22678b400ec8568fbf9663
dervism <dm.github.fifty190@passmail.com>
2026-02-09 20:31:57 +0100

    Add `Concat` class to `functions.fn1` for concatenating nested iterables

---
+++ src/main/java/functions/fn1/Concat.java
@@ -0,0 +1,31 @@
+package functions.fn1;
+
+import functions.fn.F1;
+
+import functions.iter.Iterables;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Concat<A> implements F1<Iterable<? extends Iterable<A>>, Iterable<A>> {
+
+    private static final Concat<?> INSTANCE = new Concat<>();
+
+    private Concat() {}
+
+    @Override
+    public Iterable<A> checkedApply(Iterable<? extends Iterable<A>> ass) {
+        return Iterables.stream(ass)
+                .flatMap(Iterables::stream)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A> Concat<A> concat() {
+        return (Concat<A>) INSTANCE;
+    }
+
+    public static <A> Iterable<A> concat(Iterable<? extends Iterable<A>> ass) {
+        return Concat.<A>concat().apply(ass);
+    }
+}

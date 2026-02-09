49aa5399a91b1bbc4e4796808227fd6902481fe2
dervism <dm.github.fifty190@passmail.com>
2026-02-09 20:32:16 +0100

    Add `ConcatMap` class to `functions.fn2` for mapping and flattening iterables

---
+++ src/main/java/functions/fn2/ConcatMap.java
@@ -0,0 +1,35 @@
+package functions.fn2;
+
+import functions.fn.F1;
+import functions.fn.F2;
+import functions.iter.Iterables;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConcatMap<A, B> implements F2<F1<? super A, ? extends Iterable<B>>, Iterable<A>, Iterable<B>> {
+
+    private static final ConcatMap<?, ?> INSTANCE = new ConcatMap<>();
+
+    private ConcatMap() {}
+
+    @Override
+    public Iterable<B> checkedApply(F1<? super A, ? extends Iterable<B>> f, Iterable<A> as) {
+        return Iterables.stream(as)
+                .flatMap(a -> Iterables.stream(f.apply(a)))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, B> ConcatMap<A, B> concatMap() {
+        return (ConcatMap<A, B>) INSTANCE;
+    }
+
+    public static <A, B> F1<Iterable<A>, Iterable<B>> concatMap(F1<? super A, ? extends Iterable<B>> f) {
+        return ConcatMap.<A, B>concatMap().apply(f);
+    }
+
+    public static <A, B> Iterable<B> concatMap(F1<? super A, ? extends Iterable<B>> f, Iterable<A> as) {
+        return ConcatMap.<A, B>concatMap(f).apply(as);
+    }
+}

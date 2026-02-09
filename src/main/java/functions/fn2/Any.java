edc3b74b235da3da1ea996c3541c4ab49ecbbe60
dervism <dm.github.fifty190@passmail.com>
2026-02-09 20:31:45 +0100

    Add `Any` class to `functions.fn2` for evaluating if any element matches a predicate

---
+++ src/main/java/functions/fn2/Any.java
@@ -0,0 +1,31 @@
+package functions.fn2;
+
+import functions.fn.F1;
+import functions.fn.F2;
+import functions.iter.Iterables;
+
+public class Any<A> implements F2<F1<? super A, ? extends Boolean>, Iterable<A>, Boolean> {
+
+    private static final Any<?> INSTANCE = new Any<>();
+
+    private Any() {}
+
+    @Override
+    public Boolean checkedApply(F1<? super A, ? extends Boolean> predicate, Iterable<A> as) {
+        return Iterables.stream(as)
+                .anyMatch(a -> Boolean.TRUE.equals(predicate.apply(a)));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A> Any<A> any() {
+        return (Any<A>) INSTANCE;
+    }
+
+    public static <A> F1<Iterable<A>, Boolean> any(F1<? super A, ? extends Boolean> predicate) {
+        return Any.<A>any().apply(predicate);
+    }
+
+    public static <A> Boolean any(F1<? super A, ? extends Boolean> predicate, Iterable<A> as) {
+        return Any.<A>any(predicate).apply(as);
+    }
+}
